d179d0c98fe7216a22265e44af89aae2df76d366
Abhishek <abhi144k@gmail.com>
2021-08-22 19:34:43 +0530

    ip

--- app/Http/Middleware/RestrictIpMiddleware.php
+++ app/Http/Middleware/RestrictIpMiddleware.php
@@ -6,6 +6,8 @@ use Closure;
 
 class RestrictIpMiddleware
 {
+
+    public $restrictIps = ['65.1.137.90', '127.0.0.1'];
     /**
      * Handle an incoming request.
      *
@@ -15,6 +17,9 @@ class RestrictIpMiddleware
      */
     public function handle($request, Closure $next)
     {
+        if (in_array($request->ip(), $this->restrictIps)) {
+            return response()->json(['message' => "You don't have permission to access this website."]);
+        }
         return $next($request);
     }
 }
